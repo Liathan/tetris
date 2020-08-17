6d29aed5ee102d90aba7d7ea682a5b838405586d
Liathan <48481192+Liathan@users.noreply.github.com>
2020-08-18 00:40:46 +0200

    Aggiunto salvataggio dei punteggi e gestione record
    Il salvataggio viene fatto in chiaro in un .txt nella stessa cartella, quindi facile barare, però alla fine chissene

--- logic.py
+++ logic.py
@@ -3,6 +3,8 @@ import random as rnd
 import tkinter as tk
 
 window = tk.Tk()
+window.geometry("+100+110") # Forse da rendere dinamico sulla dimensione dello schermo
+board = tk.Frame(width = 400, height = 800,master = window, relief = tk.GROOVE, borderwidth = 5)
 info = tk.Frame(width = 150, height = 800, master = window)
 pointNumLbl = tk.Label(text = "0", master = info, font = ("Helvetica", 15))
 linesNumLbl = tk.Label(text = "0", master = info, font = ("Helvetica", 15))
@@ -31,6 +33,62 @@ def cleanCanvas(canvas):
     for id in canvas.find_all():
         canvas.delete(id)
 #
+#Fare in modo che tenga solo i migliori 10 in ordine
+def insertRecord(window, name, nameScores, index):
+
+    nameScores.insert(index, name.strip() + " " + str(points) + "\n")
+    recFile = open("record.txt","w")
+    for i in range(min(10, len(nameScores))):
+        recFile.write(nameScores[i])
+    recFile.flush()
+    recFile.close()
+    window.destroy()
+
+def recordPopUp():
+    recFile = open("record.txt","r")
+    nameScores = [line for line in recFile]
+    names = [nameScore.split(" ")[0] for nameScore in nameScores]
+    scores = [int(nameScore.split(" ")[1]) for nameScore in nameScores]
+    i = 0
+    while i < len(scores) and points <= scores[i]:
+        i += 1
+    record = tk.Toplevel(width = 300, height = 400)
+    record.grab_set()
+    if i < 10:
+        recFile.close()
+        tmpLabel = tk.Label(text = "New Record", master = record)
+        nameEntry = tk.Entry(master = record, width = 50)
+        enterButton = tk.Button(master = record, text = "Enter")
+        tmpLabel.pack(pady = 5)
+        nameEntry.pack(padx = 10, pady = 5)
+        nameEntry.focus_set()
+        enterButton.pack(pady = 5)
+        nameEntry.bind("<Return>", lambda event: insertRecord(record, nameEntry.get() if nameEntry.get().strip() != "" else "unknown", nameScores, i))
+        enterButton.bind("<1>", lambda event: insertRecord(record, nameEntry.get() if nameEntry.get().strip() != "" else "unknown", nameScores, i))
+    else:
+        recFile.close()
+        tmpLabel = tk.Label(text = "Not Enough", master = record)
+        tableFrame = tk.Frame(master = record)
+        tableFrame.columnconfigure([0,1,2],minsize = 50)
+        namesLbls = [tk.Label(master = tableFrame, text = names[i]) for i in range(len(names))]
+        scoreLbls = [tk.Label(master = tableFrame, text = scores[i]) for i in range(len(scores))]
+        tmpLabel.pack()
+        for i in range(len(names)):
+            tmp = tk.Label(master = tableFrame, text = str(i + 1))
+            tmp.grid(row = i, column = 0)
+            namesLbls[i].grid(row = i, column = 1)
+            scoreLbls[i].grid(row = i, column = 2)
+        tableFrame.pack()
+        enterButton = tk.Button(master = record, text = "OK")#, command = record.destroy)
+        enterButton.bind("<Return>", lambda _ : record.destroy())
+        enterButton.bind("<1>", lambda _ : record.destroy())
+        enterButton.focus_set()
+        enterButton.pack()
+    positionX = board.winfo_rootx() + board.winfo_reqwidth() // 2 - record.winfo_reqwidth() // 2
+    positionY = board.winfo_rooty() + board.winfo_reqheight() // 2 - record.winfo_reqheight() // 2
+    record.geometry("+{}+{}".format(positionX,positionY))
+
+
 def gameOver(canvas, nextCanvas):
     print("Lost")
     global afterID
@@ -48,9 +106,9 @@ def gameOver(canvas, nextCanvas):
     canvas.create_text(200, 200, text = "GAME OVER", font = ("Impact", 60), fill = "white")
     canvas.create_text(200, 300, text = "New Game?", font = ("Impact", 45), fill = "white")
     canvas.create_text(200, 400, text = "Press Any Key", font = ("Impact", 45), fill = "gold")
-
-
     canvas.bind("<Key>",lambda event : newGame(canvas, nextCanvas))
+    recordPopUp()
+
 
 def newGame(mainCanvas, nextCanvas):
     mainCanvas.unbind("<Key>")
@@ -101,7 +159,7 @@ def updatePointLvlLines(howMany):
     global linesCleared, points, level, TICKPERSECOND
     linesCleared += howMany
     multiplier = {1: 1, 2: 2.5, 3: 7.5, 4: 30}
-    points += (level + 1) * 40 * multiplier[howMany]
+    points += int((level + 1) * 40 * multiplier[howMany])
     level = linesCleared // 10
     TICKPERSECOND = max(level,1)
     linesNumLbl.config(text = str(linesCleared))
@@ -130,12 +188,9 @@ def stopPiece(canvas, nextCanvas):
         return True
     return False
 
-# BUG: Quando faccio game over e il gameOver è stato chiamato da fall e reinizio subito fa due tick anzichè uno WTF
-# Questo perchè lo scheduling del prossimo tick viene fatto in tick, e quindi esiste ancora se faccio in fall
 
 def tick(canvas, nextCanvas, window):
     global mat, afterID
-    print("tick")
     lost  = False
     canvas.move("falling",0,25)
     if not checkInBound(canvas) or checkHitOtherPiece(canvas):
@@ -157,6 +212,10 @@ def checkHitOtherPiece(canvas):
         if mat[int(coord[1]) // 25][int(coord[0]) // 25] == 1:
             return True
     return False
+
+
+
+
 #Maybe: Controllare dove va fuori e gestire di conseguenza
 # Del tipo: se nella rotate va fuori a destra, muoverlo a sinistra fino a che è dentro
 def checkInBound(canvas):
